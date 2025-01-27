33e65e0042bffa79bcff234c0da67b58f9f1247f
Steven <sgschneider@crimson.ua.edu>
2025-01-27 17:53:04 -0600

    Add project files.

---
+++ SP2025-Assignment1-sschneider/Program.cs
@@ -0,0 +1,16 @@
+namespace SP2025_Assignment1_sschneider
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run();
+        }
+    }
+}
